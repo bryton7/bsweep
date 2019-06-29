214e167b7e889f3e10e04958f8a4bcefcc3a4b93
Bryton <hayes.bryton@gmail.com>
2019-06-29 18:34:32 -0500

    Fix indexing and print generated mines

--- bsweep.cpp
+++ bsweep.cpp
@@ -15,16 +15,16 @@ using namespace std;
 class minesweep
 {
 public:
-    bool mines_array[MS_WIDTH][MS_HEIGHT];
-    int touching[MS_WIDTH][MS_HEIGHT];
-    bool seen[MS_WIDTH][MS_HEIGHT];
+    bool mines_array[MS_HEIGHT][MS_WIDTH];
+    int touching[MS_HEIGHT][MS_WIDTH];
+    bool seen[MS_HEIGHT][MS_WIDTH];
 };
 
 void clear_map(minesweep *m) {
     int i, j;
 
     for (i = 0; i < MS_HEIGHT; i++) {
-        for (j = 0; j < MS_HEIGHT; j++) {
+        for (j = 0; j < MS_WIDTH; j++) {
             m->mines_array[i][j] = 0;
             m->touching[i][j] = 0;
             m->seen[i][j] = 0;
@@ -32,8 +32,35 @@ void clear_map(minesweep *m) {
     }
 }
 
-void init_map(minesweep *m) {
+void place_mine(minesweep *m) {
+    bool placed = 0;
+    int x_pos, y_pos;
+    while (!placed) {
+        x_pos = rand() % MS_WIDTH;
+        y_pos = rand() % MS_HEIGHT;
+        if (!m->mines_array[y_pos][x_pos]) {
+            placed = 1;
+            m->mines_array[y_pos][x_pos] = 1;
+        }
+    }
+}
 
+void init_map(minesweep *m) {
+    int i;
+    for (i = 0; i < NUM_MINES; i++) {
+        place_mine(m);
+    }
+}
+
+void render_mines_map(minesweep *m) {
+    int i, j;
+    for (i = 0; i < MS_HEIGHT; i++) {
+        for (j = 0; j < MS_WIDTH; j++) {
+            if (m->mines_array[j][i]) {
+                mvprintw(j, i, "*");
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -52,6 +79,11 @@ int main(int argc, char *argv[])
     init_pair(COLOR_WHITE, COLOR_WHITE, COLOR_BLACK); 
 
     wchar_t in;
+    minesweep *m = new minesweep;
+
+    clear_map(m);
+    init_map(m);
+    render_mines_map(m);
 
     do {
         in = getch();
